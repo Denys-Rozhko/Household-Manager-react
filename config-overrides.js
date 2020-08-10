f2b273dbbb32e6dba2bc1efed20b8b4611b00e9f
= <=>
2020-08-10 22:55:27 +0300

    Initial commit, added alias "@" for "src" folder

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+const path = require("path");
+
+module.exports = function override(config, env) {
+  config.resolve = {
+    alias : {
+      "@" : path.resolve(__dirname, "src/")
+    }
+  };
+
+  return config;
+};
